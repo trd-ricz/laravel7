6af9784bb08ddbb8776e1e1efdc0198b246851ef
trd-ricz <trd.ricz@gmail.com>
2020-03-06 12:00:40 +0800

    add files

--- laravelxx/resources/js/app.js
+++ laravelxx/resources/js/app.js
@@ -1,1 +1,12 @@
+import Vue from 'vue'
+import store from './store'
+import Home from './components/Home'
+import Posts from './components/Posts'
+
 require('./bootstrap');
+
+new Vue({
+  el: '#app',
+  store,
+  components: {Home,Posts}
+})
