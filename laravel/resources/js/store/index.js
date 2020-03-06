345e01fefad5b3676020240155d03ad7312a9fd7
trd-ricz <trd.ricz@gmail.com>
2020-03-06 16:43:12 +0800

    add latest laradock

--- laravel/resources/js/store/index.js
+++ laravel/resources/js/store/index.js
@@ -4,10 +4,6 @@ import Posts from './modules/Post'
 
 Vue.use(Vuex)
 
-
-console.log({Posts})
-
-
 export default new Vuex.Store({
 	modules: {
 		posts : Posts
---
+++ laravel/resources/views/top.blade.php
@@ -0,0 +1,45 @@
+@extends('master')
+
+@section('vuejs')
+    <script src=" {{ mix('js/app.js') }} "></script>
+@stop
+
+@section('contents')
+	@php
+
+	$value = [
+		[
+			"row_id" => 1,
+			"content" => "LoremIpsum",
+			"author" => "Ricsheil",
+			"date" => "today"
+		],
+		[
+			"row_id" => 2,
+			"content" => "Wolly",
+			"author" => "Ricsheil",
+			"date" => "today"
+		],
+		[
+			"row_id" => 3,
+			"content" => "Eva",
+			"author" => "Ricsheil",
+			"date" => "today"
+		],
+		[
+			"row_id" => 4,
+			"content" => "Hello darkness",
+			"author" => "Ricsheil",
+			"date" => "today"
+		]
+	];
+	$default_post = json_encode($value);
+
+	@endphp
+	<div id="app">
+		<home></home>
+		<posts :default-posts="{{$default_post}}"></posts>
+	</div>
+	
+
+@stop
